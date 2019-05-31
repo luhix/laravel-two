9d1e3b8348f812dfdeb517c765a4d749c664e650
luhix <zhishiniao@126.com>
2019-05-31 15:51:26 +0800

    找回密码成功消息提示

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
+    
+    
     /*
     |--------------------------------------------------------------------------
     | Password Reset Controller
@@ -35,5 +38,11 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    protected function sendResetResponse(Request $request, $response)
+    {
+        session()->flash('success', '密码更新成功，您已成功登录！');
+        return redirect($this->redirectPath());
     }
 }
