56558ff6737d583001ff428abdb1c0274cd09402
luhix <zhishiniao@126.com>
2019-06-02 17:48:27 +0800

    用户可以发表评论

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
         //
        \Carbon\Carbon::setLocale('zh'); //时间的转换
         \App\Models\Topic::observe(\App\Observers\TopicObserver::class);  //注册观察者服务
+        \App\Models\Reply::observe(\App\Observers\ReplyObserver::class);  //注册观察者服务
     }
 }
